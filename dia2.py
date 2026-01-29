e7327e8e98514b0031970a4aea7e3b22fe8170b0
Jhonatan <orejuelaaguiarjhoanatan@gmail.com>
2026-01-29 17:21:28 -0500

    Mi primer programa en Python

---
+++ dia2.py
@@ -0,0 +1,6 @@
+nombre= input("Nombre: ")
+edad= input("Edad: ")
+ciudad= input("ciudad: ")
+profesion= input("profesion: ")
+
+print("Hola, me llamo", nombre, "Tengo", edad, "años, vivo en", ciudad, "y soy", profesion)
